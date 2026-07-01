2ecfbe6c8ca1fe9d3b163ea607aa03eb4ff528ef
Amélie CAILLET-RICHARD <a.cailletrichard@gmail.com>
2026-07-01 15:09:54 +0200

    Feature/3 créer le repository backend (#7)
    * repositories
    
    * repositories
    
    * clean repo

---
+++ backend/src/main/java/co/simplon/backend/Repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+package co.simplon.backend.Repositories;
+
+import co.simplon.backend.Entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface CategoryRepository extends JpaRepository<Category, UUID> {
+}
---
+++ backend/src/main/java/co/simplon/backend/Repositories/ResourceRepository.java
@@ -0,0 +1,12 @@
+package co.simplon.backend.Repositories;
+
+import co.simplon.backend.Entities.Category;
+import co.simplon.backend.Entities.Resource;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface ResourceRepository extends JpaRepository<Resource, UUID> {
+    List<Resource> findAllByCategory(Category category);
+}
